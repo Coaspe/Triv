68e9c8cace360b7431506c68e053ee28c47e981d
Coaspe <aspalt85@gmail.com>
2025-02-05 20:49:58 +0900

    design: Add logo

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navigation = () => {
   return (
     <>
-      {/* ... 기존 코드 ... */}
+      <div className="flex justify-center mb-4 pt-8">
+        <Link href="/" className="cursor-pointer">
+          <Image src="/images/unnamed.jpg" alt="Triv" width={400} height={400} priority />
+        </Link>
+      </div>
       <nav className="flex justify-center mb-16 pt-4 text-xs">
         <ul className="flex space-x-8">
           <li>

11c8ac1e21d07ff79693b7c97ecab7fe3e540281
Coaspe <aspalt85@gmail.com>
2025-02-10 21:17:04 +0900

    design: Add No data available animation

--- components/EmptyState.tsx
+++ components/EmptyState.tsx
@@ -1,10 +1,12 @@
 import { FaRegFolderOpen } from "react-icons/fa";
 
-const EmptyState = () => (
-  <div className="flex flex-col items-center justify-center py-20 text-gray-500">
-    <FaRegFolderOpen className="w-16 h-16 mb-4" />
-    <h3 className="mb-2 text-lg font-medium">No data available 📭</h3>
-  </div>
-);
+const EmptyState = () => {
+  return (
+    <div className="flex flex-col items-center justify-center py-20 text-gray-500">
+      <FaRegFolderOpen className="w-16 h-16 mb-4 animate-bounce-twice" />
+      <h3 className="mb-2 text-lg font-medium">No data available</h3>
+    </div>
+  );
+};
 
 export default EmptyState;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,9 +16,20 @@ export default {
           "85%": { transform: "translateX(-20%)" }, // 중간에서 느리게
           "100%": { transform: "translateX(100%)" }, // 오른쪽 끝은 빨리
         },
+        "bounce-twice": {
+          "0%, 100%": {
+            transform: "translateY(0%)",
+            "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)",
+          },
+          "50%": {
+            transform: "translateY(-25%)",
+            "animation-timing-function": "cubic-bezier(0, 0, 0.2, 1)",
+          },
+        },
       },
       animation: {
         slide: "slide 2s ease-in-out infinite", // ease-in-out 으로 속도 변화 주기
+        "bounce-twice": "bounce-twice 1s ease-in-out 2",
       },
       plugins: [],
     },
